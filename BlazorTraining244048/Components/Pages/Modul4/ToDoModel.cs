a9ee9b79f407406f2f9a2282d520c25fb27222c0
Hannes Preishuber <hannesp@ppedv.de>
2024-11-13 12:45:47 +0100

    TO View Model Viewmodel Http Client Rest Service DI Container

---
+++ BlazorTraining244048/Components/Pages/Modul4/ToDoModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorTraining244048.Components.Pages.Modul4
+{
+    public class ToDoModel
+    {
+
+  
+        public Todo[] todos { get; set; }
+        public int total { get; set; }
+        public int skip { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class Todo
+    {
+        public int id { get; set; }
+        public string todo { get; set; }
+        public bool completed { get; set; }
+        public int userId { get; set; }
+    }
+
+}
---
+++ BlazorTraining244048/Components/Pages/Modul4/toDoVM.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorTraining244048.Components.Pages.Modul4
+{
+    public class ToDoVM
+    {
+        public ToDoModel TodoO;
+        public ToDoVM(IHttpClientFactory _http)
+        {
+            using (var client= _http.CreateClient())
+            {
+                TodoO = client.GetFromJsonAsync<ToDoModel>("https://dummyjson.com/todos").Result;
+            }
+        }
+    }
+}
