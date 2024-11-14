848f0980532e7602d005e52298105cf804d104a0
Hannes Preishuber <hannesp@ppedv.de>
2024-11-14 11:50:46 +0100

    Bootstrap CSS Fix

---
+++ BlazorTraining244048/Components/Pages/Modul5/BootstrapErrorFixFieldClassCSSProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTraining244048.Components.Pages.Modul5
+{
+    public class BootstrapErrorFixFieldClassCSSProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+           bool isInValid= editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isInValid ? "is-invalid" : "is-valid";
+        }
+    }
+}
