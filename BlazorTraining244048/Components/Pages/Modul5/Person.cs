ce2d5cfa5edcbf8a435aebe458632e3e1b88a3b8
Hannes Preishuber <hannesp@ppedv.de>
2024-11-14 11:06:50 +0100

    DataAnnotations als Grundlage von Validierungsregeln in Poco defininieren

---
+++ BlazorTraining244048/Components/Pages/Modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTraining244048.Components.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10,MinimumLength =3, ErrorMessage ="zwischen 3 und 10 Zeichen")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(100,190, ErrorMessage ="zwischen 100 und 190, sonst tot")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+    }
+}
