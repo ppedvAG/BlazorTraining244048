cd50284b66d63879b02e4ae3814c9fb0534bad38
Hannes Preishuber <hannesp@ppedv.de>
2024-11-15 10:48:23 +0100

    Scafffoling CRUD Template

--- BlazorTraining244048/Program.cs
+++ BlazorTraining244048/Program.cs
@@ -1,4 +1,4 @@
-using BlazorTraining244048.Components;
+﻿using BlazorTraining244048.Components;
 using BlazorTraining244048.Components.Pages.Modul4;
 using BlazorTraining244048.Models;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +12,11 @@ builder.Services.AddSingleton<ChatVM>();
 builder.Services.AddScoped<ToDoVM>();
 builder.Services.AddHttpClient();
 builder.Services.AddDbContext<NorthwindContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("northwind")));
+builder.Services.AddDbContextFactory<NorthwindContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("northwind")),
+    ServiceLifetime.Scoped);
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +25,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
